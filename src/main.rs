48e36f1e0888cff25874e0ebd6a8d8a230ea2a04
ChaoticGood <colete_nico@yahoo.com>
2023-08-29 16:05:12 +0800

    Initial setup to make wasm work

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use bevy::{prelude::*, window::PresentMode};
+use cfg_if::cfg_if;
+
+use multithread;
+
+fn main() {
+  let mut app = App::new();
+  app
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+      primary_window: Some(Window {
+        title: "Ironverse Editor".into(),
+        resolution: (800., 600.).into(),
+        present_mode: PresentMode::AutoVsync,
+        fit_canvas_to_parent: true,
+        prevent_default_event_handling: false,
+        ..default()
+      }),
+      ..default()
+    }));
+  
+  cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+      multithread::run();
+    }
+  }
+
+}
