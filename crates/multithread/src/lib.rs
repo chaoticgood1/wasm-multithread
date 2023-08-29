5aa92a8dba0d922e1a3732efb4656a57a37498c7
ChaoticGood <colete_nico@yahoo.com>
2023-08-29 23:21:54 +0800

    Working sample of raytrace-parallel as a crate

--- crates/multithread/src/lib.rs
+++ crates/multithread/src/lib.rs
@@ -5,94 +5,164 @@ pub fn run() {
 }
 
 
-// #![feature(async_closure)]
+use futures_channel::oneshot;
+use js_sys::{Promise, Uint8ClampedArray, WebAssembly};
+use rayon::prelude::*;
+use wasm_bindgen::prelude::*;
 
-// use wasm_mt::prelude::*;
-// use wasm_mt::Thread;
-// use wasm_mt::utils::console_ln;
+macro_rules! console_log {
+    ($($t:tt)*) => (crate::log(&format_args!($($t)*).to_string()))
+}
 
-// use wasm_bindgen::prelude::*;
-// use wasm_bindgen_futures::spawn_local;
+mod pool;
 
-// #[wasm_bindgen]
-// pub fn app() {
-//     spawn_local(async move {
-//         let pkg_js = "./pkg/wasm_multithread.js"; // path to `wasm-bindgen`'s JS binding
-//         let mt = WasmMt::new(pkg_js).and_init().await.unwrap();
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn logv(x: &JsValue);
+}
 
-//         let th = mt.thread().and_init().await.unwrap();
+#[wasm_bindgen]
+pub struct Scene {
+    inner: raytracer::scene::Scene,
+}
 
-//         let _ = run_closure(&th).await;
-//         let _ = run_async_closure(&th).await;
-//         let _ = run_js(&th).await;
-//         let _ = run_async_js(&th).await;
-//     });
-// }
+#[wasm_bindgen]
+impl Scene {
+    /// Creates a new scene from the JSON description in `object`, which we
+    /// deserialize here into an actual scene.
+    #[wasm_bindgen(constructor)]
+    pub fn new(object: JsValue) -> Result<Scene, JsValue> {
+        console_error_panic_hook::set_once();
+        Ok(Scene {
+            inner: serde_wasm_bindgen::from_value(object)
+                .map_err(|e| JsValue::from(e.to_string()))?,
+        })
+    }
 
-// //
+    /// Renders this scene with the provided concurrency and worker pool.
+    ///
+    /// This will spawn up to `concurrency` workers which are loaded from or
+    /// spawned into `pool`. The `RenderingScene` state contains information to
+    /// get notifications when the render has completed.
+    pub fn render(
+        self,
+        concurrency: usize,
+        pool: &pool::WorkerPool,
+    ) -> Result<RenderingScene, JsValue> {
+        let scene = self.inner;
+        let height = scene.height;
+        let width = scene.width;
 
-// fn add(a: i32, b: i32) -> i32 { a + b }
+        // Allocate the pixel data which our threads will be writing into.
+        let pixels = (width * height) as usize;
+        let mut rgb_data = vec![0; 4 * pixels];
+        let base = rgb_data.as_ptr() as usize;
+        let len = rgb_data.len();
 
-// async fn run_closure(th: &Thread) -> Result<(), JsValue> {
-//     let a = 1;
-//     let b = 2;
-//     let ans = exec!(th, move || {
-//         let c = add(a, b);
+        // Configure a rayon thread pool which will pull web workers from
+        // `pool`.
+        let thread_pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(concurrency)
+            .spawn_handler(|thread| {
+                pool.run(|| thread.run()).unwrap();
+                Ok(())
+            })
+            .build()
+            .unwrap();
 
-//         Ok(JsValue::from(c))
-//     }).await?;
-//     assert_eq!(ans, JsValue::from(3));
-//     console_ln!("run_closure ans: {:?}", ans);
+        // And now execute the render! The entire render happens on our worker
+        // threads so we don't lock up the main thread, so we ship off a thread
+        // which actually does the whole rayon business. When our returned
+        // future is resolved we can pull out the final version of the image.
+        let (tx, rx) = oneshot::channel();
+        pool.run(move || {
+            thread_pool.install(|| {
+                rgb_data
+                    .par_chunks_mut(4)
+                    .enumerate()
+                    .for_each(|(i, chunk)| {
+                        let i = i as u32;
+                        let x = i % width;
+                        let y = i / width;
+                        let ray = raytracer::Ray::create_prime(x, y, &scene);
+                        let result = raytracer::cast_ray(&scene, &ray, 0).to_rgba();
+                        chunk[0] = result.data[0];
+                        chunk[1] = result.data[1];
+                        chunk[2] = result.data[2];
+                        chunk[3] = result.data[3];
+                    });
+            });
+            drop(tx.send(rgb_data));
+        })?;
 
-//     Ok(())
-// }
+        let done = async move {
+            match rx.await {
+                Ok(_data) => Ok(image_data(base, len, width, height).into()),
+                Err(_) => Err(JsValue::undefined()),
+            }
+        };
 
-// //
+        Ok(RenderingScene {
+            promise: wasm_bindgen_futures::future_to_promise(done),
+            base,
+            len,
+            height,
+            width,
+        })
+    }
+}
 
-// use wasm_mt::utils::sleep;
-// async fn sub(a: i32, b: i32) -> i32 {
-//     sleep(1000).await;
-//     a - b
-// }
+#[wasm_bindgen]
+pub struct RenderingScene {
+    base: usize,
+    len: usize,
+    promise: Promise,
+    width: u32,
+    height: u32,
+}
 
-// async fn run_async_closure(th: &Thread) -> Result<(), JsValue> {
-//     let a = 1;
-//     let b = 2;
-//     let ans = exec!(th, async move || {
-//         let c = sub(a, b).await;
+// Inline the definition of `ImageData` here because `web_sys` uses
+// `&Clamped<Vec<u8>>`, whereas we want to pass in a JS object here.
+#[wasm_bindgen]
+extern "C" {
+    pub type ImageData;
 
-//         Ok(JsValue::from(c))
-//     }).await?;
-//     assert_eq!(ans, JsValue::from(-1));
-//     console_ln!("run_async_closure ans: {:?}", ans);
+    #[wasm_bindgen(constructor, catch)]
+    fn new(data: &Uint8ClampedArray, width: f64, height: f64) -> Result<ImageData, JsValue>;
+}
 
-//     Ok(())
-// }
+#[wasm_bindgen]
+impl RenderingScene {
+    /// Returns the JS promise object which resolves when the render is complete
+    pub fn promise(&self) -> Promise {
+        self.promise.clone()
+    }
 
-// //
+    /// Return a progressive rendering of the image so far
+    #[wasm_bindgen(js_name = imageSoFar)]
+    pub fn image_so_far(&self) -> ImageData {
+        image_data(self.base, self.len, self.width, self.height)
+    }
+}
 
-// async fn run_js(th: &Thread) -> Result<(), JsValue> {
-//     let ans = exec_js!(th, "
-//         const add = (a, b) => a + b;
-//         return add(1, 2);
-//     ").await?;
-//     assert_eq!(ans, JsValue::from(3));
-//     console_ln!("run_js ans: {:?}", ans);
-
-//     Ok(())
-// }
-
-// //
-
-// async fn run_async_js(th: &Thread) -> Result<(), JsValue> {
-//     let ans = exec_js_async!(th, "
-//         const sub = (a, b) => new Promise(resolve => {
-//             setTimeout(() => resolve(a - b), 1000);
-//         });
-//         return await sub(1, 2);
-//     ").await?;
-//     assert_eq!(ans, JsValue::from(-1));
-//     console_ln!("run_async_js ans: {:?}", ans);
-
-//     Ok(())
-// }
+fn image_data(base: usize, len: usize, width: u32, height: u32) -> ImageData {
+    // Use the raw access available through `memory.buffer`, but be sure to
+    // use `slice` instead of `subarray` to create a copy that isn't backed
+    // by `SharedArrayBuffer`. Currently `ImageData` rejects a view of
+    // `Uint8ClampedArray` that's backed by a shared buffer.
+    //
+    // FIXME: that this may or may not be UB based on Rust's rules. For example
+    // threads may be doing unsynchronized writes to pixel data as we read it
+    // off here. In the context of wasm this may or may not be UB, we're
+    // unclear! In any case for now it seems to work and produces a nifty
+    // progressive rendering. A more production-ready application may prefer to
+    // instead use some form of signaling here to request an update from the
+    // workers instead of synchronously acquiring an update, and that way we
+    // could ensure that even on the Rust side of things it's not UB.
+    let mem = wasm_bindgen::memory().unchecked_into::<WebAssembly::Memory>();
+    let mem = Uint8ClampedArray::new(&mem.buffer()).slice(base as u32, (base + len) as u32);
+    ImageData::new(&mem, width as f64, height as f64).unwrap()
+}
