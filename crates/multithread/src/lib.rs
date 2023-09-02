de7f8299e31c7055425d488b6778bd3ba3a2ed08
ChaoticGood <colete_nico@yahoo.com>
2023-09-02 15:37:17 +0800

    Artificially limiting threads to use

--- crates/multithread/src/lib.rs
+++ crates/multithread/src/lib.rs
@@ -3,7 +3,7 @@
 use wasm_mt_pool::prelude::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
-use wasm_mt::utils::{console_ln, fetch_as_arraybuffer};
+use wasm_mt::utils::{console_ln, fetch_as_arraybuffer, sleep};
 use js_sys::ArrayBuffer;
 use voxels::chunk::chunk_manager::*;
 use flume::{Sender, Receiver};
@@ -15,17 +15,20 @@ pub mod plugin;
 
 #[wasm_bindgen]
 pub fn app() {
-  let (send, recv) = flume::unbounded();
+  let (send_queue, recv_queue) = flume::unbounded();
+  let (send_process, recv_process) = flume::unbounded();
+  let (send_to_wasm, recv_to_wasm) = flume::unbounded();
   spawn_local(async move {
     let ab_js = fetch_as_arraybuffer("crates/multithread/pkg/multithread.js").await.unwrap();
     let ab_wasm = fetch_as_arraybuffer("crates/multithread/pkg/multithread_bg.wasm").await.unwrap();
-    run(recv, ab_js, ab_wasm).await.unwrap();
+    run(recv_process, send_to_wasm, ab_js, ab_wasm).await.unwrap();
   });
 
-  recv_key_sender(send);
+  recv_key_from_wasm(send_queue);
+  process_queued_keys(recv_queue, send_process, recv_to_wasm);
 }
 
-fn recv_key_sender(send: Sender<[i64; 3]>) {
+fn recv_key_from_wasm(send: Sender<[i64; 3]>) {
   let callback = Closure::wrap(Box::new(move |event: CustomEvent | {
     let data = event.detail().as_string().unwrap();
     let bytes = array_bytes::hex2bytes(data).unwrap();
@@ -39,7 +42,7 @@ fn recv_key_sender(send: Sender<[i64; 3]>) {
     let key: [i64; 3] = a[0..3].try_into().unwrap();
     let _ = send.send(key);
 
-    console_ln!("recv key {:?}", key);
+    // console_ln!("recv key {:?}", key);
   }) as Box<dyn FnMut(CustomEvent)>);
 
   let window = web_sys::window().unwrap();
@@ -51,39 +54,44 @@ fn recv_key_sender(send: Sender<[i64; 3]>) {
   callback.forget();
 }
 
-
 pub async fn run(
   recv: Receiver<[i64; 3]>,
+  send_to_wasm: Sender<[i64; 3]>,
   ab_js: ArrayBuffer, 
   ab_wasm: ArrayBuffer
 ) -> Result<(), JsValue> {
-  let size = 2;
+  let size = 8;
   let pool = ThreadPool::new_with_arraybuffers(size, ab_js, ab_wasm)
     .and_init().await?;
-  
+
   while let Ok(msg) = recv.recv_async().await {
+    let s = send_to_wasm.clone();
     let cb = move |result: Result<JsValue, JsValue>| {
       let r = result.unwrap();
       let ab = r.dyn_ref::<js_sys::ArrayBuffer>().unwrap();
       let vec = js_sys::Uint8Array::new(ab);
-
+  
       let bytes = vec.to_vec();
       let octree = Octree {
         key: msg,
         data: bytes,
       };
+      
       let encoded: Vec<u8> = bincode::serialize(&octree).unwrap();
       let str = array_bytes::bytes2hex("", encoded);
 
       let window = web_sys::window().unwrap();
       let _ = window.post_message(&JsValue::from_str(&str), "/");
-    };
 
+      let _ = s.send(msg);
+    };
+  
     pool_exec!(pool, move || {
       let data = compute_voxel(msg);
       Ok(wasm_mt::utils::u8arr_from_vec(&data).buffer().into())
     }, cb);
   }
+
   Ok(())
 }
 
@@ -91,4 +99,47 @@ fn compute_voxel(key: [i64; 3]) -> Vec<u8> {
   let manager = ChunkManager::default();
   let chunk = ChunkManager::new_chunk(&key, 4, 4, manager.noise);
   chunk.octree.data
+}
+
+
+fn process_queued_keys(
+  recv_queue: Receiver<[i64; 3]>, 
+  send_process: Sender<[i64; 3]>,
+  recv_to_wasm: Receiver<[i64; 3]>,
+) {
+
+  let fps = 60;
+  let sleep_time = 1000 / fps;
+  spawn_local(async move {
+    let max_threads = 8;
+    let mut current_threads = 0;
+    let mut queued_keys = Vec::new();
+
+    loop {
+      for key in recv_queue.drain() {
+        queued_keys.push(key);
+
+        // console_ln!("queued_keys.len() {}", queued_keys.len());
+      }
+
+      if queued_keys.len() > 0 && current_threads < max_threads {
+        // console_ln!("1queued {} current {}", queued_keys.len(), current_threads);
+
+        let key = queued_keys.pop().unwrap();
+        let _ = send_process.send(key);
+        current_threads += 1;
+
+        // console_ln!("process {:?}: {}", key, current_threads);
+      }
+
+      for k in recv_to_wasm.drain() {
+        current_threads -= 1;
+        // console_ln!("drain {:?}: {}", k, current_threads);
+      }
+
+      // console_ln!("2queued {} current {}", queued_keys.len(), current_threads);
+
+      sleep(sleep_time).await;
+    }
+  });
 }
--- crates/multithread/src/plugin.rs
+++ crates/multithread/src/plugin.rs
@@ -31,6 +31,12 @@ fn init(
   window
     .set_onmessage(Some(cb2.as_ref().unchecked_ref()));
   cb2.forget();
+
+
+  for i in 0..20000 {
+    let key = [0, -1, 0];
+    send_key(key);
+  }
 }
 
 fn update(
@@ -39,35 +45,40 @@ fn update(
 ) {
   for bytes in local_res.recv.drain() {
     // info!("update() {:?}", bytes);
+    info!("wasm recieved");
     
     let octree: Octree = bincode::deserialize(&bytes[..]).unwrap();
-    info!("bevy recv {:?}", octree.data);
+    // info!("bevy recv {:?}", octree.data);
   }
 
   if local_res.timer.tick(time.delta()).just_finished() {
-    info!("send key");
+    // info!("send key");
 
-    let key: Vec<i64> = vec![0, -1, 0];
-      
-    let k: Vec<[u8; 8]> = key.iter().map(|a| a.to_be_bytes()).collect();
-    let mut bytes = Vec::new();
-    for k1 in k.iter() {
-      bytes.append(&mut k1.to_vec());
-    }
-    let str = array_bytes::bytes2hex("", &bytes);
-
-    // console_ln!("b {:?}", bytes.len());
-    // console_ln!("str {:?}", str);
-
-    // let k = String::from_utf8_lossy(&key);
-    let e = CustomEvent::new_with_event_init_dict(
-      "key", CustomEventInit::new().detail(&JsValue::from_str(&str))
-    ).unwrap();
-
-    let window = web_sys::window().unwrap();
-    let _ = window.dispatch_event(&e);
+    // for i in 0..20 {
+    //   let key = [0, -1, 0];
+    //   send_key(key);
+    // }
+    
   }
 }
+
+
+fn send_key(key: [i64; 3]) {
+  let k: Vec<[u8; 8]> = key.iter().map(|a| a.to_be_bytes()).collect();
+  let mut bytes = Vec::new();
+  for k1 in k.iter() {
+    bytes.append(&mut k1.to_vec());
+  }
+  let str = array_bytes::bytes2hex("", &bytes);
+
+  let e = CustomEvent::new_with_event_init_dict(
+    "key", CustomEventInit::new().detail(&JsValue::from_str(&str))
+  ).unwrap();
+
+  let window = web_sys::window().unwrap();
+  let _ = window.dispatch_event(&e);
+}
+
 
 #[derive(Resource)]
 struct LocalResource {
@@ -82,7 +93,7 @@ impl Default for LocalResource {
     Self {
       send: send,
       recv: recv,
-      timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+      timer: Timer::from_seconds(100.0, TimerMode::Repeating),
     }
   }
 }
